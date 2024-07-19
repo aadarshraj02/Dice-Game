1fd4ebbe5bca2b881cb8ce139bdf6891a90d890a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-19 12:52:24 +0530

    added function to start gameplay

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import StartGame from "./components/StartGame";
 
 function App() {
+  const [isGameStatrted, setGameStarted] = useState(false);
+  const toggleGamePlay = () => {
+    setGameStarted((prev) => !prev);
+  };
+
   return (
     <div>
       <StartGame></StartGame>
