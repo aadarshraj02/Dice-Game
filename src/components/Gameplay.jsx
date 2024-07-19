fb784a455243957632e0c554b096570a741c4924
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-19 17:27:49 +0530

    applied display flex to header section

--- src/components/Gameplay.jsx
+++ src/components/Gameplay.jsx
@@ -1,13 +1,22 @@
 import NumberSelector from "./NumberSelector";
 import TotalScore from "./TotalScore";
+import styled from "styled-components";
 
 const Gameplay = () => {
   return (
-    <main>
-      <TotalScore></TotalScore>
-      <NumberSelector></NumberSelector>
-    </main>
+    <MainContainer>
+      <div className="headerSection">
+        <TotalScore></TotalScore>
+        <NumberSelector></NumberSelector>
+      </div>
+    </MainContainer>
   );
 };
 
 export default Gameplay;
+
+const MainContainer = styled.main`
+  .headerSection {
+    display: flex;
+  }
+`;
