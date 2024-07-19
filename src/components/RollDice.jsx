aa41bee855c09afe9e76a44a610289487f9e17d3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-19 17:39:09 +0530

    added dice conatainer

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -1,5 +1,9 @@
+import styled from "styled-components";
+
 function RollDice() {
-  return <div></div>;
+  return <DiceContainer></DiceContainer>;
 }
 
 export default RollDice;
+
+const DiceContainer = styled.div``;
