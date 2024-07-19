539ab425f5504d000f8611798c4bde6098aa062f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-19 17:43:28 +0530

    styled p

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -16,5 +16,13 @@ export default RollDice;
 const DiceContainer = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
   margin-top: 48px;
+
+  p {
+    font-size: 24px;
+    font-weight: 500;
+    margin-top: 24px;
+  }
 `;
