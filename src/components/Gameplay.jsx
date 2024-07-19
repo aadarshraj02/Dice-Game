82d609a966690140d17e9bec938bad130775da54
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-19 13:13:01 +0530

    imported Numberselector.jsx

--- src/components/Gameplay.jsx
+++ src/components/Gameplay.jsx
@@ -1,9 +1,11 @@
+import NumberSelector from "./NumberSelector";
 import TotalScore from "./TotalScore";
 
 const Gameplay = () => {
   return (
     <main>
       <TotalScore></TotalScore>
+      <NumberSelector></NumberSelector>
     </main>
   );
 };
