53199b4f859365d1c10f17a75eaf80d1924f45ce
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-19 18:04:04 +0530

    updated props

--- src/components/Gameplay.jsx
+++ src/components/Gameplay.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import NumberSelector from "./NumberSelector";
 import RollDice from "./RollDice";
 import TotalScore from "./TotalScore";
 import styled from "styled-components";
 
 const Gameplay = () => {
+  const [selectedNumber, setSelectedNumber] = useState();
   return (
     <MainContainer>
       <div className="headerSection">
         <TotalScore></TotalScore>
-        <NumberSelector></NumberSelector>
+        <NumberSelector
+          selectedNumber={selectedNumber}
+          setSelectedNumber={setSelectedNumber}
+        ></NumberSelector>
       </div>
       <RollDice></RollDice>
     </MainContainer>
--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
+
 import styled from "styled-components";
 
-const NumberSelector = () => {
+const NumberSelector = ({ selectedNumber, setSelectedNumber }) => {
   const arrayNum = [1, 2, 3, 4, 5, 6];
-  const [selectedNumber, setSelectedNumber] = useState();
 
   return (
     <NumberSelectorContainer>
