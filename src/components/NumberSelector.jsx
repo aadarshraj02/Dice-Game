18d0e8c99b788f1345ee6d129bbd79eaaf3e4449
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-19 16:36:34 +0530

    added box div and styled box div

--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+
 const NumberSelector = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Box>1</Box>
+      <Box>2</Box>
+      <Box>3</Box>
+      <Box>4</Box>
+      <Box>5</Box>
+      <Box>6</Box>
+    </div>
+  );
 };
 
 export default NumberSelector;
+
+const Box = styled.div`
+  height: 72px;
+  width: 72px;
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  font-weight: 700;
+  border-radius: 5px;
+`;
