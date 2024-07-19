b32b1fc89ce1accc7a60c009a50c34bd1754d0c5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-19 18:56:49 +0530

    applied styling on rules container

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -19,4 +19,17 @@ const Rules = () => {
 
 export default Rules;
 
-const RulesContainer = styled.div``;
+const RulesContainer = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  background-color: #fbf1f1;
+  padding: 20px;
+  margin-top: 40px;
+  border-radius: 10px;
+  h2 {
+    font-size: 24px;
+  }
+  .text {
+    margin-top: 24px;
+  }
+`;
