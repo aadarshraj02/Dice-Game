53ba80e5a1b7df5cf91d4ba4ab9b6dbf2a8e2695
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-19 12:28:12 +0530

    added images

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
 function StartGame() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <img src="/dices.png" alt="" />
+    </Container>
+  );
 }
 
 export default StartGame;
 
-const Container = styled.div ``;
+const Container = styled.div`
+  max-width: 1180px;
+`;
